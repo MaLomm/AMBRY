a844605133f714644454c1882c756880b704eafe
MaLomm <MaLomm@users.noreply.github.com>
2016-02-17 22:09:37 +0100

    Initial Adaptation

--- utils/FORWARDPASS.m
+++ utils/FORWARDPASS.m
@@ -1,5 +1,5 @@
-function [outputactivation, hiddenactivation] = FORWARDPASS(...
-	stimuli,exemplars,distancemetric,attentionweights,associationweights,params)
+function [responseactivation, categoryactivation, hiddenactivation] = FORWARDPASS(...
+	stimuli,exemplars,distancemetric,attentionweights,associationweights,responseweights,params)
 %--------------------------------------------------------------------------
 % This script runs a forward pass in alcove and returns the information
 % about network performance.
@@ -9,30 +9,37 @@ function [outputactivation, hiddenactivation] = FORWARDPASS(...
 %	networkinput			items to be passed through the model
 %	exemplars				coordinates of each known exemplar
 %	attentionweights		input->hidden weights
-% 	associationweights		hidden->output weights
+% 	associationweights		hidden->category weights
+%   responseweights         category->response weights
 %	distancemetric			0 for city block, 1 for euclidean
-% 	params					parameters [c,assoclearning,attenlearning,phi]
+% 	params					parameters [c,assoclearning,attenlearning,responlearning,phi]
 
 % -------------------------------------
 % --OUTPUT ARGUMENTS		DESCRIPTION
-%	outputactivation		output unit activations
+%	responseactivation		output unit activations
+%	categoryactivation		category unit activations
 %	hiddenactivation		exemplar node activations
 %--------------------------------------------------------------------------
 
 % initialize variables
 c = params(1);
+beta = params(4);
 	
 %Calculate Distances and Activation at Hidden Node
 %-----------------------------------------------------
-distances = pairdist(stimuli,exemplars,distancemetric,attentionweights);
+distances = pairdist(stimuli,exemplars,distancemetric,attentionweights, beta);
 hiddenactivation = exp((-c)*distances);
 
-% Calculates the activation at the output nodes
+% Calculates the activation at the category nodes
 %-----------------------------------------------------
-outputactivation = hiddenactivation * associationweights;
-   	
+categoryactivation = hiddenactivation * associationweights;
+
+% Calculates the activation at the category nodes
+%-----------------------------------------------------
+responseactivation = categoryactivation * responseweights;
+
 % humble teachers
-outputactivation(outputactivation> 1) =  1.0;
-outputactivation(outputactivation<-1) = -1.0;
+responseactivation(responseactivation> 1) =  1.0;
+responseactivation(responseactivation<-1) = -1.0;
 
 end
